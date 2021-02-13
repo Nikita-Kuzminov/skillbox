859ff6d54be8221691aebd36c4eeef298205e021
Nikita-Kuzminov <nkuzminov220299@gmail.com>
2021-02-13 19:58:53 +0300

    Try to make one loop from two in Tickets

---
+++ 2_module/3_lesson/Tickets/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        // Печатает в консоль номера билетов от 200 000 до 210 000 и от 220 000 до 235 000 с использованием for and
+        // while
+
+        int x = 200000;
+        int y = 220000;
+        for (int i = 1; i < 10002; i++) {
+            System.out.println(x);
+            x++;
+            System.out.println(y);
+            y++;
+        }
+
+//        for (int n = 220000; n < 235001; n++) {
+//            System.out.println(n);
+//        }
+//
+//        int j = 200000;
+//        while (j < 210001) {
+//            System.out.println(j);
+//            j++;
+//        }
+//
+//        int k = 220000;
+//        while (k < 235001) {
+//            System.out.println(k);
+//            k++;
+//        }
+    }
+}
