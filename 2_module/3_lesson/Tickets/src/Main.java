e9878f6f032a465cb00580c358b97bf109f20c6d
Nikita-Kuzminov <nkuzminov220299@gmail.com>
2021-02-13 20:22:29 +0300

    Combining two cycles while in one

--- 2_module/3_lesson/Tickets/src/Main.java
+++ 2_module/3_lesson/Tickets/src/Main.java
@@ -3,17 +3,32 @@ public class Main {
         // Печатает в консоль номера билетов от 200 000 до 210 000 и от 220 000 до 235 000 с использованием for and
         // while
 
-        int x = 200000;
-        int y = 220000;
-        for (int i = 0; i < 30000; i++) {
-            if (i < 10001) {
-                System.out.println(x);
-                x++;
+//        int x = 200000;
+//        int y = 220000;
+//        for (int i = 0; i < 30000; i++) {
+//            if (i < 10001) {
+//                System.out.println(x);
+//                x++;
+//            }
+//            if (i > 19998) {
+//                System.out.println(y);
+//                y++;
+//            }
+//        }
+
+        int k = 0;
+        int m = 200000;
+        int n = 220000;
+        while (k < 30000) {
+            if (k < 10001) {
+                System.out.println(m);
+                m++;
             }
-            if (i > 19998) {
-                System.out.println(y);
-                y++;
+            if (k > 19998) {
+                System.out.println(n);
+                n++;
             }
+            k++;
         }
 
 //        for (int n = 220000; n < 235001; n++) {
