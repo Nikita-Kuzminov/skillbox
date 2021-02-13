1145a40abdf6f4f1c59ee6aa7b071d6c93cbae4b
Nikita-Kuzminov <nkuzminov220299@gmail.com>
2021-02-13 16:03:06 +0300

    Change structure of directories

--- second_module/fourth_lesson/Tickets/src/Main.java
+++ second_module/fourth_lesson/Tickets/src/Main.java
@@ -1,3 +1,26 @@
 public class Main {
+    public static void main(String[] args) {
+        // Печатает в консоль номера билетов от 200 000 до 210 000 и от 220 000 до 235 000 с использованием for and
+        // while
 
+        for (int i = 200000; i < 210001; i++) {
+            System.out.println(i);
+        }
+
+        int j = 200000;
+        while (j < 210001) {
+            System.out.println(j);
+            j++;
+        }
+
+        for (int n = 220000; n < 235001; n++) {
+            System.out.println(n);
+        }
+
+        int k = 220000;
+        while (k < 235001) {
+            System.out.println(k);
+            k++;
+        }
+    }
 }
